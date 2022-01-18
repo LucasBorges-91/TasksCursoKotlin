d6fc5f97f462cd3670b93174946db4d0cc7d0295
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-18 18:30:30 -0300

    Criando os arquivos taskModel e PriorityModel, mapeando todos os endpoints da API

--- app/src/main/java/com/example/tasks/service/listener/APIListener.kt
+++ app/src/main/java/com/example/tasks/service/listener/APIListener.kt
@@ -1,10 +1,10 @@
 package com.example.tasks.service.listener
 
-import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.model.HeaderModel
 
 interface APIListener {
 
-    fun onSucess( model: HeaderModel )
+    fun onSucess( model: HeaderModel)
 
     fun onFailure( str: String )
 }
